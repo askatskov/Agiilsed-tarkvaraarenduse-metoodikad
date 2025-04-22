00b4626bc105b53da4487c44bf329f36b9aa2933
Opilane TTHK <opilane@tthk.ee>
2025-04-22 13:52:50 +0300

    done

--- task.py
+++
@@ -1,7 +0,0 @@
-class Task:
-    def __init__(self, title, status="to-do"):
-        self.title = title
-        self.status = status
-
-    def mark_done(self):
-        self.status = "done"
--- test_tasks.py
+++
@@ -1,6 +0,0 @@
-from task import Task
-
-def test_mark_done():
-    task = Task("Testi ülesanne")
-    task.mark_done()
-    assert task.status == "done"
